e63181b0fbcb7b3c5fc9071c13e95c16cdfcd1cb
Erharuyi <godwinerharuyi>
2025-10-27 23:39:59 +0100

    Updated Docker setup for HNG Stage 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "Starting Nginx with PORT=${PORT}"
 
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT} ${ACTIVE_POOL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
